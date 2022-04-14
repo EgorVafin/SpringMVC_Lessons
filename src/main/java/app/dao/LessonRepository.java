4454df97f0fc47d80abac945e8be125e22b13e89
egor-vafin <post1978@gmail.com>
2022-04-14 11:26:30 +0300

    test commit

--- src/main/java/app/dao/LessonRepository.java
+++ src/main/java/app/dao/LessonRepository.java
@@ -4,6 +4,7 @@ import app.entity.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
 }
