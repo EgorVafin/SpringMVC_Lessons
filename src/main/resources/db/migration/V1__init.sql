01ee0426362b796543bf97f16f11e19d224025c3
egor-vafin <post1978@gmail.com>
2022-05-14 16:07:55 +0300

    temp files

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,34 @@
+drop table if exists lessons;
+drop table if exists persons;
+drop table if exists subscriptions;
+alter table lessons drop foreign key FKffnkqqgifjm9agaxusyrmjsl;
+alter table subscriptions drop foreign key FKpbh3yob6tb0s5e042o369jnhv;
+alter table subscriptions drop foreign key FKlqlsoks080lmc0929ggfl99od;
+
+
+create table lessons (
+    id bigint not null auto_increment,
+    comment varchar(255),
+    lesson_date time,
+    lesson_desc varchar(255),
+    duration integer,
+    home_work varchar(255),
+    teacher_id bigint,
+    primary key (id));
+
+create table persons (
+    id bigint not null auto_increment,
+    email varchar(255),
+    name varchar(255),
+    password varchar(255),
+    surname varchar(255),
+    primary key (id));
+
+create table subscriptions (
+    lesson_id bigint not null,
+    person_id bigint not null);
+
+alter table persons add constraint UK_1x5aosta48fbss4d5b3kuu0rd unique (email);
+alter table lessons add constraint FKffnkqqgifjm9agaxusyrmjsl foreign key (teacher_id) references persons (id);
+alter table subscriptions add constraint FKpbh3yob6tb0s5e042o369jnhv foreign key (person_id) references persons (id);
+alter table subscriptions add constraint FKlqlsoks080lmc0929ggfl99od foreign key (lesson_id) references lessons (id);
